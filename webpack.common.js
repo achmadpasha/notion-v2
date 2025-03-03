e0d7f916cfe4ebcb4fbd58496b06c166d5824f59
achmadpasha <nabilahmadpasha@gmail.com>
2025-03-03 14:59:43 +0800

    changing webpack output directory

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     create: './src/js/pages/create.js'
   },
   output: {
-    path: path.resolve(__dirname, 'bundle'),
+    path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js'
   },
 };
